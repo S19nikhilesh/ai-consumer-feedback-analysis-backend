22e3ce3f04d77decbdb4e8f3bc6d8a0b603f8e8a
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-09-02 09:48:56 +0530

    implement user authentication

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const cors=require('cors');
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 require("dotenv").config();
 connectDB();
@@ -9,6 +10,9 @@ const app=express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
 
 app.get('/',(req,res)=>{
     res.json({
